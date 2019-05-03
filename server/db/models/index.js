f0cf5a09b1c2df8ada06e7f60611656643edf200
Marcia <mwan0906@gmail.com>
2019-05-03 15:41:19 -0400

    basic model definitions

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,6 @@
-const User = require('./user')
+const User = require('./user');
+const Location = require('./location');
+const Label = require('./label');
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -13,6 +15,22 @@ const User = require('./user')
  * for example, we can say: const {User} = require('../db/models')
  * instead of: const User = require('../db/models/user')
  */
+
+Location.belongsToMany(Label, { through: 'LocationLabel' });
+Label.belongsToMany(Location, { through: 'LocationLabel' });
+
+Location.prototype.getLabels = async () => {
+  let labels = [];
+  await Label.findAll({
+    include: [{ model: Location }],
+    where: {
+      locationId: this.id
+    }
+  }).then(foundLabels => foundLabels.forEach(label => labels.push(label.name)));
+};
+
 module.exports = {
-  User
-}
+  User,
+  Location,
+  Label
+};
