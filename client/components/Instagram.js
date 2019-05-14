5959be298c3fbbce94c5c404428a005bbad9b3a6
Saeed Sheikh <saeedshaikh166@gmail.com>
2019-05-14 11:52:57 -0400

    Removes reference to old file in instagram.js

--- client/components/Instagram.js
+++ client/components/Instagram.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { instagramThunk } from '../store/instagramImages';
+import { instagramThunk } from '../store/instagram';
 import Slider from 'react-slick';
 
 class Instagram extends Component {
