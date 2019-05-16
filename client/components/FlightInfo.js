c256987fa5d5f00354729c18d64bb8fe22f33282
Marcia <mwan0906@gmail.com>
2019-05-16 13:52:16 -0400

    dumbed down FlightInfo screen, added all flight data to url of results screen, flights retrieved on tripinfoform screen

--- client/components/FlightInfo.js
+++ client/components/FlightInfo.js
@@ -1,143 +1,64 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 
-class FlightInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.makeFlightsArr = this.makeFlightsArr.bind(this);
-  }
-  makeFlightsArr() {
-    const {
-      returning,
-      departing,
-      destinations,
-      instagramLocs,
-      instagramUser,
-      budget,
-      seats
-    } = this.props;
-
-    let destinationChoices = {};
-    if (instagramUser) {
-      destinationChoices = instagramLocs;
-    } else {
-      destinationChoices = destinations;
-    }
-    let totalFlightsArr = [];
-
-    for (let airportCode in destinationChoices) {
-      if (destinationChoices.hasOwnProperty(airportCode)) {
-        let locationName = destinationChoices[airportCode].name;
-        locationName = locationName[0].toUpperCase() + locationName.slice(1);
-        let flightsDeparting = departing[airportCode] || [];
-        let flightsReturning = returning[airportCode] || [];
-        if (flightsDeparting)
-          flightsDeparting = flightsDeparting
-            .filter(({ price }) => Number(price) <= (budget / (2 * seats)))
-            .sort((a, b) => Number(b.price) - Number(a.price))
-            .slice(0, 5);
-        if (flightsReturning)
-          flightsReturning = flightsReturning
-            .filter(({ price }) => Number(price) <= (budget / (2 * seats)))
-            .sort((a, b) => Number(b.price) - Number(a.price))
-            .slice(0, 5);
-
-        if (flightsDeparting.length && flightsReturning.length) {
-          // const totalPrices = flightsDeparting.map((departure, idx) => {
-          //   if (departure) {
-          //     let sum =
-          //       parseFloat(departure.price, 10) +
-          //       parseFloat(flightsReturning[idx].price, 10);
-          //     return sum.toFixed(2);
-          //   }
-          // });
-          let flightsObj = {
-            city: locationName,
-            departing: flightsDeparting,
-            returning: flightsReturning
-            // totals: totalPrices
-          };
-          totalFlightsArr.push(flightsObj);
-        }
-      }
-    }
-    return totalFlightsArr;
-  }
-
-  render() {
-    const flightsInfoArr = this.makeFlightsArr();
-    if (!flightsInfoArr.length) {
-      return (
-        <div className="ui centered medium header">
-          There are no flights at this price
-        </div>
-      );
-    }
+const FlightInfo = props => {
+  const { flightsInfoArr } = props;
+  if (!flightsInfoArr.length) {
     return (
-      <div className="flights">
-        {flightsInfoArr.map(element => {
-          return (
-            <div key={element.city}>
-              <div>
-                <label className="ui large blue header">{element.city}</label>
-              </div>
-              <label role="list" className="ui list container">
-                <label className="ui medium header">
-                  <div className="icon">
-                    <i className="fas fa-plane-departure" />
-                  </div>
-                  Departing
-                </label>
-
-                {element.departing.map((departure, i) => {
-                  return (
-                    <div
-                      key={i}
-                      role="listitem"
-                      className="ui container segment"
-                    >
-                      <div className="content">
-                        <div>Airline: {departure.carrier}</div>
-                        <div>Class: {departure.class}</div>
-                        <div>${departure.price} / seat</div>
-                      </div>
-                    </div>
-                  );
-                })}
-                <label className="ui medium header">
-                  <div className="icon">
-                    <i className="fas fa-plane-arrival" />
-                  </div>
-                  Returning
-                </label>
-                {element.returning.map((returns, i) => {
-                  return (
-                    <div
-                      key={i}
-                      role="listitem"
-                      className="ui container segment"
-                    >
-                      <div className="content">
-                        <div>Airline: {returns.carrier}</div>
-                        <div>Class: {returns.class}</div>
-                        <div>${returns.price} / seat</div>
-                      </div>
-                    </div>
-                  );
-                })}
-              </label>
-            </div>
-          );
-        })}
+      <div className="ui centered medium header">
+        There are no flights at this price
       </div>
     );
   }
-}
-const mapState = state => ({
-  returning: state.location.returning,
-  departing: state.location.departing,
-  destinations: state.destinations.destinationInfo,
-  instagramLocs: state.instagram.locations,
-  instagramUser: state.user.instagramId
-});
-export default connect(mapState)(FlightInfo);
+  return (
+    <div className="flights">
+      {flightsInfoArr.map(element => {
+        return (
+          <div key={element.city}>
+            <div>
+              <label className="ui large blue header">{element.city}</label>
+            </div>
+            <label role="list" className="ui list container">
+              <label className="ui medium header">
+                <div className="icon">
+                  <i className="fas fa-plane-departure" />
+                </div>
+                Departing
+              </label>
+
+              {element.departing.map((departure, i) => {
+                return (
+                  <div key={i} role="listitem" className="ui container segment">
+                    <div className="content">
+                      <div>Airline: {departure.carrier}</div>
+                      <div>Class: {departure.class}</div>
+                      <div>${departure.price} / seat</div>
+                    </div>
+                  </div>
+                );
+              })}
+              <label className="ui medium header">
+                <div className="icon">
+                  <i className="fas fa-plane-arrival" />
+                </div>
+                Returning
+              </label>
+              {element.returning.map((returns, i) => {
+                return (
+                  <div key={i} role="listitem" className="ui container segment">
+                    <div className="content">
+                      <div>Airline: {returns.carrier}</div>
+                      <div>Class: {returns.class}</div>
+                      <div>${returns.price} / seat</div>
+                    </div>
+                  </div>
+                );
+              })}
+            </label>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default FlightInfo;
--- client/components/resultscreen.js
+++ client/components/resultscreen.js
@@ -3,61 +3,146 @@ import ResultsMap from './ResultsMap';
 import BudgetBar from './portableBudgetBar';
 import queryString from 'query-string';
 import FlightInfo from './FlightInfo';
+import { connect } from 'react-redux';
+import history from '../history';
 
 class ResultScreen extends React.Component {
   constructor() {
     super();
     this.state = {
       budget: 1000,
-      flights: [], // should be calculated and then passed down
       coords: [[]],
-      seats: 1
+      seats: 1,
+      departing: {},
+      returning: {},
+      destinations: {}
     };
     this.onBudgetChange = this.onBudgetChange.bind(this);
+    this.makeFlightsArr = this.makeFlightsArr.bind(this);
   }
 
   componentDidMount() {
-    const { budget, coords, seats } = queryString.parse(this.props.location.search);
+    const {
+      budget,
+      coords,
+      seats,
+      departing,
+      returning,
+      destinations
+    } = queryString.parse(this.props.location.search);
+
+    const realBudget = Number(budget);
     const rawCoords = coords.split(',');
     const realCoords = [];
     while (rawCoords.length) {
       realCoords.push([rawCoords.shift(), rawCoords.shift()]);
     }
+    const realSeats = Number(seats);
+    const realDeparting = JSON.parse(departing);
+    const realReturning = JSON.parse(returning);
+    const realDestinations = JSON.parse(destinations);
+
     this.setState({
-      budget: Number(budget),
+      budget: realBudget,
       coords: realCoords,
-      seats: Number(seats)
+      seats: realSeats,
+      departing: realDeparting,
+      returning: realReturning,
+      destinations: realDestinations
     });
   }
 
   onBudgetChange(e) {
+    const newBudget = Number(e.target.value);
     this.setState({
-      budget: e.target.value
+      budget: newBudget
     });
+    const {
+      coords,
+      seats,
+      departing,
+      returning,
+      destinations
+    } = queryString.parse(this.props.location.search);
+    history.replace(
+      `/results?seats=${seats}&budget=${newBudget}&coords=${coords}&departing=${departing}&returning=${returning}&destinations=${destinations}`
+    );
+  }
+
+  makeFlightsArr(budget, seats, departing, returning, destinations) {
+    let totalFlightsArr = [];
+
+    for (let airportCode in destinations) {
+      if (destinations.hasOwnProperty(airportCode)) {
+        let locationName = destinations[airportCode].name;
+        locationName = locationName[0].toUpperCase() + locationName.slice(1);
+        let flightsDeparting = departing[airportCode] || [];
+        let flightsReturning = returning[airportCode] || [];
+        if (flightsDeparting)
+          flightsDeparting = flightsDeparting
+            .filter(({ price }) => Number(price) <= budget / (2 * seats))
+            .sort((a, b) => Number(b.price) - Number(a.price))
+            .slice(0, 5);
+        if (flightsReturning)
+          flightsReturning = flightsReturning
+            .filter(({ price }) => Number(price) <= budget / (2 * seats))
+            .sort((a, b) => Number(b.price) - Number(a.price))
+            .slice(0, 5);
+
+        if (flightsDeparting.length && flightsReturning.length) {
+          let flightsObj = {
+            city: locationName,
+            departing: flightsDeparting,
+            returning: flightsReturning
+          };
+          totalFlightsArr.push(flightsObj);
+        }
+      }
+    }
+    return totalFlightsArr;
   }
 
   render() {
-//    console.log(this.props);
+    const {
+      budget,
+      coords,
+      seats,
+      departing,
+      returning,
+      destinations
+    } = this.state;
+    const flights = this.makeFlightsArr(
+      budget,
+      seats,
+      departing,
+      returning,
+      destinations
+    );
     return (
       <div className="ui padded grid">
         <div className="ui padded grid eleven wide column">
           <div className="centered two column row">
-            <BudgetBar
-              budget={this.state.budget}
-              onChange={this.onBudgetChange}
-            />
+            <BudgetBar budget={budget} onChange={this.onBudgetChange} />
           </div>
           <div className="row">
-            <ResultsMap coords={this.state.coords} />
+            <ResultsMap coords={coords} />
           </div>
         </div>
         {/* and then the list of flights to the side somewhere here */}
         <div className="four wide column">
-          <FlightInfo budget={this.state.budget} seats={this.state.seats} />
+          <FlightInfo budget={budget} seats={seats} flightsInfoArr={flights} />
         </div>
       </div>
     );
   }
 }
 
-export default ResultScreen;
+const mapState = state => ({
+  returning: state.location.returning,
+  departing: state.location.departing,
+  destinationChoices: state.user.instagramId
+    ? state.instagram.locations
+    : state.destinations.destinationInfo
+});
+
+export default connect(mapState)(ResultScreen);
